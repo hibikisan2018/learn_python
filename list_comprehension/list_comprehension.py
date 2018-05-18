fbab5c2b4810a8d8bdc7a038a8166ce0b473e059
hibikisan2017 <hibikisan2010@gmail.com>
2018-05-19 08:33:27 +0900

    code modified

--- list_comprehension/list_comprehension.py
+++ list_comprehension/list_comprehension.py
@@ -4,33 +4,60 @@
 Created on Sun May 13 18:56:20 2018
 @author: hibikisan
 """
-def makelist01():
+def makelist01(n):
     a = []
-    for i in range(N):
+    for i in range(n):
         a.append(2*i+1)
     return a
 
-def makelist02():
-    return list(map(lambda x:2*x+1, range(N)))
+def makelist02(n):
+    return list(map(lambda x:2*x+1, range(n)))
 
-def makelist03():
-    return [2*n+1 for n in range(N)]
+def makelist03(n):
+    return [2*n_+1 for n_ in range(n)]
 
 if __name__ == '__main__':
     import time
-    N = 10**7
-    time0 = time.time()
+    import math
+    import matplotlib.pyplot as plt
+   
+    #Set the number 
+    x = range(1, 7)
+   
+    timelist_01 = []
+    timelist_02 = []
+    timelist_03 = []
     
-    list01 = makelist01()
-    time1 = time.time()
-       
-    list02 = makelist02()
-    time2 = time.time()
+    for i in x:
+        n = pow(10, i)
 
-    list03 = makelist03()
-    time3 = time.time()
+        #Start time
+        time0 = time.time()
+        list01 = makelist01(n)
+        time1 = time.time()
+           
+        list02 = makelist02(n)
+        time2 = time.time()
+    
+        list03 = makelist03(n)
+        time3 = time.time()
+        
+        #Store the processing time of each medhod
+        timelist_01.append(time1 - time0)
+        timelist_02.append(time2 - time1)
+        timelist_03.append(time3 - time2)
 
-    print('for loop\t\t:{}'.format(time1-time0))
-    print('map function\t\t:{}'.format(time2-time1))
-    print('list comprihension\t:{}'.format(time3-time2))
+        print('for loop\t\t:{}'.format(time1 - time0))
+        print('map function\t\t:{}'.format(time2 - time1))
+        print('list comprihension\t:{}'.format(time3 - time2))
+        
+    #Draw graph with logarithmic scale
+    plt.plot(x, [math.log10(x) for x in timelist_01], label = 'for loop')
+    plt.plot(x, [math.log10(x) for x in timelist_02], label = 'map function')
+    plt.plot(x, [math.log10(x) for x in timelist_03], label = 'list comprehension')
+
+    plt.xlabel("log10(N)")
+    plt.ylabel("log10(Time) [s]")
+    plt.legend()
+    plt.show()
     
