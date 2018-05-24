5f1919c2f0ba28548771b01e21b639005f11bc80
hibikisan2017 <hibikisan2010@gmail.com>
2018-05-24 22:36:38 +0900

    revised

--- algorithm/gcd.py
+++ algorithm/gcd.py
@@ -1,25 +1,30 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sat Apr 28 23:00:10 2018
-
+Referenced from https://hibiki-press.tech/algorithm/gcd/700
 @author: hibikisan
 """
-
-def gcd(a, b):
+def iterative_gcd(a, b):
     r = a % b
     while(r != 0):
         a = b
         b = r
         r = a % b
-    
     return b
+
+def recursive_gcd(a, b):
+    r = a % b
+    if r == 0:
+        return b
+    else:
+        return recursive_gcd(b, a % b)
 
 if __name__ == '__main__':
     num = input("Enter 2 numbers separated by space. (ex: 24 36) ")
     num_ = list(map(int, num.split(' ')))
     a, b = num_[0], num_[1]
-
-    n = gcd(a, b)
-    print('The greatest common divisor of {} and {} is {}'.format(a, b, n))
+    
+    print('The greatest common divisor of {} and {} is'.format(a, b))
+    print('Iterative:{}'.format(iterative_gcd(a, b)))
+    print('Recursive:{}'.format(recursive_gcd(a, b)))
     
