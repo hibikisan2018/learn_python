7cc2baaca68308970f328a47e95c814c1d7a62dd
hibikisan2017 <hibikisan2010@gmail.com>
2019-05-01 02:05:36 +0900

    modified code

--- setdefault/letter_counter.py
+++ setdefault/letter_counter.py
@@ -12,15 +12,9 @@ def charcounter(l):
         count_[s] += 1
     return count_
     
-
-if __name__ == '__main__':
-  
-    #message_ = 'This dog helped him watch the sheep.'
-    message_ = 'Timmie Willie is a country mouse who is accidentally transported to a city in a vegetable basket. When he wakes up, he finds himself in a party and makes a friend. When he is unable to bear the city life, he returns to his home but invites his friend to the village. When his friend visits him, something similar happens.'
-
-    message = message_.split(' ')
-    count = charcounter(message)
-    print(count)
+message = 'This dog helped him watch the sheep.'
+count = charcounter(message)
+print(count)
     
     
 
